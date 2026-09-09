09dc618534f766e254447bccde144e4e42f915eb
knkbrh2023-max <knkbrh2023@gmail.com>
2026-09-09 13:14:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.JA_CONFIG = {
     "https://ihbdrtnkfitytklonnel.supabase.co/functions/v1",
 
   razorpayKeyId:
-    "rzp_test_TXeg61WItGnzFQ",
+    "rzp_test_TYoGOC4jNFpbQE",
 
   siteUrl:
     "https://knkbrh2023-max.github.io/jyotish-assam/",
